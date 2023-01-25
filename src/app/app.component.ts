1dbd530caff54510e0a1c675ef49c0507171355f
Vivekanand Rajbhar <vivekanand.rb9@gmail.com>
2023-01-26 01:54:55 +0530

    css to sass config change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { PanchangService } from './panchang.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'vedic-panchang';
