2556f92e640feee198f2c97427252edbfc3fb1c3
Vivekanand Rajbhar <vivekanand.rb9@gmail.com>
2023-03-18 00:10:01 +0530

    location modal css updated

--- src/app/shared/components/location-modify/location-modify.component.ts
+++ src/app/shared/components/location-modify/location-modify.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-
+import { LocationService } from '@shared/services/location.service';
 @Component({
   selector: 'panchang-location-modify-component',
   templateUrl: './location-modify.component.html',
@@ -8,11 +8,14 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class LocationModifyComponent implements OnInit {
 
   public display = "none";
+  public locationObj: { [key: string]: any } = {};
   @Output() closed:EventEmitter<boolean> = new EventEmitter<boolean>();
-  constructor() { }
+  constructor(private _locationService: LocationService) { }
 
   ngOnInit(): void {
     this.display = "block";
+    this.locationObj = this._locationService.getLocationObj();
+    // ^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$
   }
 
   onCloseHandled() {
